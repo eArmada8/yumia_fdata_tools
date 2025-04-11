efef99fdd1b02318631f43c5064263c44326025e
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2025-04-10 19:17:32 -0700

    Speed improvement, remove dependency

--- ktid/yumia_find_kidsobjdb_using_ktid.py
+++ ktid/yumia_find_kidsobjdb_using_ktid.py
@@ -3,17 +3,36 @@
 # all the references in that ktid file.  It will skip over files that cannot be properly
 # read by my tool.
 #
-# Requires yumia_decode_kidsobjdb.py, place in the same folder as this script.
-#
 # GitHub eArmada8/yumia_fdata_tools
 
 try:
     import json, struct, os, sys, glob
-    from yumia_decode_kidsobjdb import *
 except ModuleNotFoundError as e:
     print("Python module missing! {}".format(e.msg))
     input("Press Enter to abort.")
     raise   
+
+def read_kidsobjdb_for_hashes (kidsobjdb_file):
+    with open(kidsobjdb_file, 'rb') as f:
+        f.seek(0,2)
+        eof = f.tell()
+        f.seek(0x0)
+        problems = []
+        magic = f.read(8)
+        assert magic == b'_DOK0000'
+        entry_size, platform, num_entries = struct.unpack("<3I", f.read(12))
+        kidsobjdb_namefile_hash, file_size = struct.unpack("<2I", f.read(8))
+        all_hashes = []
+        while f.tell() < eof:
+            start = f.tell()
+            magic = f.read(8)
+            assert magic in [b'IDOK0000', b'RDOK0000']
+            entry_size, prop_name, prop_typename, prop_count = struct.unpack("<4I", f.read(16))
+            all_hashes.append(prop_name)
+            f.seek(start + entry_size)
+            while not (f.tell() % 4 == 0):
+                f.seek(1,1)
+        return(all_hashes)
 
 def hunt_for_kidsobjdb_using_ktid (ktid_filename):
     ktid_hashes = []
@@ -28,11 +47,11 @@ def hunt_for_kidsobjdb_using_ktid (ktid_filename):
     matches = []
     for i in range(len(kidsobjdb_files)):
         try:
-            db_hashes = [x['name_hash'] for x in read_kidsobjdb(kidsobjdb_files[i], kidsobjdb_namefile = '', ask_for_namefile = False)]
-            if all([x in db_hashes for x in ktid_hashes]):
-                matches.append(kidsobjdb_files[i])
+            db_hashes = read_kidsobjdb_for_hashes(kidsobjdb_files[i])
         except AssertionError:
-            pass #Not all kidsobjdb files can be decoded by my tool
+            print("Skipping {} due to unknown formatting.".format(kidsobjdb_files[i]))
+        if all([x in db_hashes for x in ktid_hashes]):
+            matches.append(kidsobjdb_files[i])
     return (matches)
     
 if __name__ == "__main__":
